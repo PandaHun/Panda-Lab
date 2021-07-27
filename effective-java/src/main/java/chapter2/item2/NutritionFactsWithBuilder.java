c8b7108545ff86f4eddcca6c66562ee4e89b4406
PandaHun <gnsgnsek@gmail.com>
2021-07-27 22:36:48 +0900

    [effective_java] feat: Create Item2 example

---
+++ effective-java/src/main/java/chapter2/item2/NutritionFactsWithBuilder.java
@@ -0,0 +1,57 @@
+package chapter2.item2;
+
+public class NutritionFactsWithBuilder {
+    private final int servingSize;
+    private final int servings;
+    private final int calories;
+    private final int fat;
+    private final int sodium;
+    private final int carbohydrate;
+
+    private NutritionFactsWithBuilder(Builder builder) {
+        this.servingSize = builder.servingSize;
+        this.servings = builder.servings;
+        this.calories = builder.calories;
+        this.fat = builder.fat;
+        this.sodium = builder.sodium;
+        this.carbohydrate = builder.carbohydrate;
+    }
+
+    public static class Builder {
+        private final int servingSize;
+        private final int servings;
+        private int calories;
+        private int fat;
+        private int sodium;
+        private int carbohydrate;
+
+        public Builder(int servingSize, int servings) {
+            this.servingSize = servingSize;
+            this.servings = servings;
+        }
+
+        public Builder calories(int calories) {
+            this.calories = calories;
+            return this;
+        }
+
+        public Builder fat(int fat) {
+            this.fat = fat;
+            return this;
+        }
+
+        public Builder sodium(int sodium) {
+            this.sodium = sodium;
+            return this;
+        }
+
+        public Builder carbohydrate(int carbohydrate) {
+            this.carbohydrate = carbohydrate;
+            return this;
+        }
+
+        public NutritionFactsWithBuilder build() {
+            return new NutritionFactsWithBuilder(this);
+        }
+    }
+}
