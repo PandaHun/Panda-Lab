fcad49c2d79221ab0f2acf518bf65b3ac5c242e6
PandaHun <gnsgnsek@gmail.com>
2022-03-16 16:22:56 +0900

    [refactoring] refactor: Change local variable to methods

--- refactoring/src/main/java/com/pandahun/refactoring/chapter1/Theater.java
+++ refactoring/src/main/java/com/pandahun/refactoring/chapter1/Theater.java
@@ -7,15 +7,18 @@ import java.util.Map;
 
 public class Theater {
 
+    private Map<String, Play> plays;
+
     public String statement(Invoice invoice, Map<String, Play> plays) {
         int totalAmount = 0;
         int volumeCredits = 0;
+        this.plays = Map.copyOf(plays);
         String result = "청구 내역 (고객명: " + invoice.getCustomer() + ")\n";
         String format = "$%.2f";
 
         for (Performance perf : invoice.getPerformances()) {
-            Play play = plays.get(perf.getPlayId());
-            int thisAmount = amountFor(perf, play);
+            Play play = playFor(perf);
+            int thisAmount = amountFor(perf);
             volumeCredits += Math.max(perf.getAudience() - 30, 0);
             if (play.getType() == COMEDY) {
                 volumeCredits += Math.floor((double) perf.getAudience() / 5.0);
@@ -30,26 +33,32 @@ public class Theater {
         return result;
     }
 
-    private int amountFor(Performance perf, Play play) {
+    private int amountFor(Performance performance) {
         int result;
-        switch (play.getType()) {
+        switch (playFor(performance).getType()) {
             case TRAGEDY:
                 result = 40_000;
-                if (perf.getAudience() > 30) {
-                    result += 1000 * (perf.getAudience() - 30);
+                if (performance.getAudience() > 30) {
+                    result += 1000 * (performance.getAudience() - 30);
                 }
                 break;
             case COMEDY:
                 result = 30_000;
-                if (perf.getAudience() > 20) {
-                    result += 10_000 + 500 * (perf.getAudience() - 20);
+                if (performance.getAudience() > 20) {
+                    result += 10_000 + 500 * (performance.getAudience() - 20);
                 }
-                result += 300 * perf.getAudience();
+                result += 300 * performance.getAudience();
                 break;
             default:
                 throw new IllegalArgumentException(
-                    "알 수 없는 장르: " + play.getType().getPlayType());
+                    "알 수 없는 장르: " + playFor(performance)
+                        .getType()
+                        .getPlayType());
         }
         return result;
+    }
+
+    private Play playFor(Performance performance) {
+        return plays.get(performance.getPlayId());
     }
 }
