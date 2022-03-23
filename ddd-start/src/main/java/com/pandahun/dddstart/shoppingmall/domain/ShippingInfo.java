d40347a56254f1fd300c947789c1e480be90bd54
PandaHun <gnsgnsek@gmail.com>
2022-03-24 00:31:34 +0900

    [ddd] refactor: Apply Address in ShippingInfo

--- ddd-start/src/main/java/com/pandahun/dddstart/shoppingmall/domain/ShippingInfo.java
+++ ddd-start/src/main/java/com/pandahun/dddstart/shoppingmall/domain/ShippingInfo.java
@@ -3,31 +3,18 @@ package com.pandahun.dddstart.shoppingmall.domain;
 public class ShippingInfo {
 
     private Receiver receiver;
-    private String shippingAddress1;
-    private String shippingAddress2;
-    private String shippingZipCode;
+    private Address address;
 
-    public ShippingInfo(Receiver receiver, String shippingAddress1, String shippingAddress2,
-        String shippingZipCode) {
+    public ShippingInfo(Receiver receiver, Address address) {
         this.receiver = receiver;
-        this.shippingAddress1 = shippingAddress1;
-        this.shippingAddress2 = shippingAddress2;
-        this.shippingZipCode = shippingZipCode;
+        this.address = address;
     }
 
     public Receiver getReceiver() {
         return receiver;
     }
 
-    public String getShippingAddress1() {
-        return shippingAddress1;
-    }
-
-    public String getShippingAddress2() {
-        return shippingAddress2;
-    }
-
-    public String getShippingZipCode() {
-        return shippingZipCode;
+    public Address getAddress() {
+        return address;
     }
 }
